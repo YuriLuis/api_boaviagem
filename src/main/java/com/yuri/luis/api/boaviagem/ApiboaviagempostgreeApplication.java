5594925af85a3b723ba43388836782bfd486526b
YuriLuis <yuri.luizg@hotmail.com>
2020-06-23 15:38:17 -0300

    Projeto Criado

---
+++ src/main/java/com/yuri/luis/api/boaviagem/ApiboaviagempostgreeApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.api.boaviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiboaviagempostgreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiboaviagempostgreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/luis/api/boaviagem/ApiboaviagempostgreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.api.boaviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiboaviagempostgreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
